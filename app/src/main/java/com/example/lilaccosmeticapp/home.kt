c16b6407332650732e103df3571148972a4f0205
shehanikk <park.shehani.7@gmail.com>
2022-04-05 14:01:31 +0530

    plugin 3line

--- app/src/main/java/com/example/lilaccosmeticapp/home.kt
+++ app/src/main/java/com/example/lilaccosmeticapp/home.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_home.*
 
 class home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +15,14 @@ class home : AppCompatActivity() {
         gotoLogin.setOnClickListener(){
             val intent = Intent(this,welcomeScreen::class.java)
             startActivity(intent)
+
         }
+
+        btnCatagory.setOnClickListener(){
+            val intent = Intent(this,catogeries::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
