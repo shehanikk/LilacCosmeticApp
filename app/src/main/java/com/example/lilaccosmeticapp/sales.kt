7a088d4450084b0074f7436ead3a174166473cad
shehanikk <park.shehani.7@gmail.com>
2022-04-06 13:21:28 +0530

    Sale page is done only images have to add

--- app/src/main/java/com/example/lilaccosmeticapp/sales.kt
+++ app/src/main/java/com/example/lilaccosmeticapp/sales.kt
@@ -1,11 +1,54 @@
 package com.example.lilaccosmeticapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.activity_face.*
+import kotlinx.android.synthetic.main.activity_sales.*
 
 class sales : AppCompatActivity() {
+    lateinit var viewPager: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sales)
+
+        btnHomeSale.setOnClickListener(){
+            val intent = Intent(this,home::class.java)
+            startActivity(intent)
+        }
+
+        btnCategorySale.setOnClickListener(){
+            val intent = Intent(this,catogeries::class.java)
+            startActivity(intent)
+        }
+
+        btnSaleSale.setOnClickListener(){
+            val intent = Intent(this,sales::class.java)
+            startActivity(intent)
+        }
+
+         viewPager = findViewById(R.id.view_pager)
+
+        val fragments: ArrayList<Fragment> = arrayListOf(
+            Page1Fragment(),
+            Page2Fragment(),
+            Page3Fragment(),
+            Page4Fragment(),
+            Page5Fragment()
+        )
+
+        val adapter = ViewPagerAdapter(fragments, this)
+        viewPager.adapter = adapter
+    }
+
+    override fun onBackPressed() {
+        if (viewPager.currentItem == 0){
+        super.onBackPressed()
+    }else{
+        viewPager.currentItem = viewPager.currentItem - 1
+
+        }
     }
 }
