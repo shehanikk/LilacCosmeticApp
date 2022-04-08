c4c546b03e4358b3a60579fccd4184e4d4b8353c
shehanikk <park.shehani.7@gmail.com>
2022-04-08 22:16:29 +0530

    home page ui is finished buttons are also working

--- app/src/main/java/com/example/lilaccosmeticapp/remcollection.kt
+++ app/src/main/java/com/example/lilaccosmeticapp/remcollection.kt
@@ -1,11 +1,34 @@
 package com.example.lilaccosmeticapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_kyliecollection.*
+import kotlinx.android.synthetic.main.activity_remcollection.*
 
 class remcollection : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_remcollection)
+
+        btnHomeRem.setOnClickListener(){
+            val intent = Intent(this,home::class.java)
+            startActivity(intent)
+        }
+
+        btnCategoryRem.setOnClickListener(){
+            val intent = Intent(this,catogeries::class.java)
+            startActivity(intent)
+        }
+
+        btnSaleRem.setOnClickListener(){
+            val intent = Intent(this,sales::class.java)
+            startActivity(intent)
+        }
+
+        btnBackRem.setOnClickListener(){
+            val intent = Intent(this,home::class.java)
+            startActivity(intent)
+        }
     }
 }
