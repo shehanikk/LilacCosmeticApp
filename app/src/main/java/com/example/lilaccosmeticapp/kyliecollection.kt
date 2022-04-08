81d18ff383f5d54f5e27efd39ee65722b0433121
shehanikk <park.shehani.7@gmail.com>
2022-04-08 16:35:36 +0530

    mac collection ui is done

--- app/src/main/java/com/example/lilaccosmeticapp/kyliecollection.kt
+++ app/src/main/java/com/example/lilaccosmeticapp/kyliecollection.kt
@@ -1,11 +1,34 @@
 package com.example.lilaccosmeticapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_catogeries.*
+import kotlinx.android.synthetic.main.activity_kyliecollection.*
 
 class kyliecollection : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kyliecollection)
+
+        btnHomeKylie.setOnClickListener(){
+            val intent = Intent(this,home::class.java)
+            startActivity(intent)
+        }
+
+        btnCategoryKylie.setOnClickListener(){
+            val intent = Intent(this,catogeries::class.java)
+            startActivity(intent)
+        }
+
+        btnSaleKylie.setOnClickListener(){
+            val intent = Intent(this,sales::class.java)
+            startActivity(intent)
+        }
+
+        btnBackKylie.setOnClickListener(){
+            val intent = Intent(this,home::class.java)
+            startActivity(intent)
+        }
     }
 }
