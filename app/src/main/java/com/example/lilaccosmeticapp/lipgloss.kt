d306698a5f58c35052c8b1320cf27229283c80ef
Kalana9902 <kalanabimsaradj5@gmail.com>
2022-04-24 21:28:47 +0530

    cart adding done on lipgloss page

--- app/src/main/java/com/example/lilaccosmeticapp/lipgloss.kt
+++ app/src/main/java/com/example/lilaccosmeticapp/lipgloss.kt
@@ -3,13 +3,55 @@ package com.example.lilaccosmeticapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.lilaccosmeticapp.adapter.MyProductAdapter
+import com.example.lilaccosmeticapp.eventbus.UpdateCartEvent
+import com.example.lilaccosmeticapp.listeners.CartLoadListener
+import com.example.lilaccosmeticapp.listeners.ProductLoadListener
+import com.example.lilaccosmeticapp.models.cart
+import com.example.lilaccosmeticapp.models.product
+import com.example.lilaccosmeticapp.utils.SpaceItemDecoration
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_lipgloss.*
 import kotlinx.android.synthetic.main.activity_lips.*
+import kotlinx.android.synthetic.main.activity_lipstick.*
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
-class lipgloss : AppCompatActivity() {
+class lipgloss : AppCompatActivity(), ProductLoadListener, CartLoadListener {
+
+    lateinit var productLoadListener: ProductLoadListener
+    lateinit var cartLoadListener: CartLoadListener
+
+    override fun onStart() {
+        super.onStart()
+        EventBus.getDefault().register(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (EventBus.getDefault().hasSubscriberForEvent(UpdateCartEvent::class.java))
+            EventBus.getDefault().removeStickyEvent(UpdateCartEvent::class.java)
+        EventBus.getDefault().unregister(this)
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
+    fun onUpdateCartEvent(event: UpdateCartEvent)
+    {
+        countCartFromFirebase()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lipgloss)
+
+        init()
+        loadProductFromFirebase()
+        countCartFromFirebase()
 
         btnHomeLipgloss.setOnClickListener(){
             val intent = Intent(this,home::class.java)
@@ -35,6 +77,92 @@ class lipgloss : AppCompatActivity() {
             val intent = Intent(this,shoppingcart::class.java)
             startActivity(intent)
         }
+
+    }
+    private fun countCartFromFirebase() {
+        val cartModels : MutableList<cart> = ArrayList()
+        FirebaseDatabase.getInstance()
+            .getReference("Cart")
+            .child("UNIQUE_USER_ID")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for (cartSnapshot in snapshot.children)
+                    {
+                        val cartModel = cartSnapshot.getValue(cart::class.java)
+                        cartModel!!.key = cartSnapshot.key
+                        cartModels.add(cartModel)
+                    }
+                    cartLoadListener.onLoadCartSuccess(cartModels)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    cartLoadListener.onLoadCartFailed(error.message)
+                }
+
+            })
+    }
+
+    private fun loadProductFromFirebase() {
+        val drinkModels : MutableList<product> = ArrayList()
+        FirebaseDatabase.getInstance()
+            .getReference("Product")
+            .child("lipgloss")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists())
+                    {
+                        for(drinkSnapshot in snapshot.children)
+                        {
+                            val drinkModel = drinkSnapshot.getValue(product::class.java)
+                            drinkModel!!.key = drinkSnapshot.key
+                            drinkModels.add(drinkModel)
+                        }
+
+                        productLoadListener.onDrinkLoadSuccess(drinkModels)
+
+
+                    }
+
+                    else
+                        productLoadListener.onDrinkLoadFailed("Drink items not exists")
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    productLoadListener.onDrinkLoadFailed(error.message)
+                }
+
+            })
+    }
+    private fun init(){
+        productLoadListener = this
+        cartLoadListener = this
+
+        val gridLayoutManager = GridLayoutManager(this,2)
+        rv_lipgloss.layoutManager = gridLayoutManager
+        rv_lipgloss.addItemDecoration(SpaceItemDecoration())
+        //recycler_drink.adapter = MyDrinkAdapter(this, productList )
+
+        //btnCart.setOnClickListener{ startActivity(Intent(this,CartActivity::class.java)) }
+
+    }
+
+    override fun onDrinkLoadSuccess(drinkModelList: List<product>?) {
+        val adapter = MyProductAdapter(this,drinkModelList!!, cartLoadListener )
+        rv_lipgloss.adapter = adapter
+    }
+
+    override fun onDrinkLoadFailed(message: String?) {
+        Snackbar.make(lipglossLayout,message!!, Snackbar.LENGTH_LONG).show()
+    }
+    override fun onLoadCartSuccess(cartModelList: List<cart>) {
+        var cartSum = 0
+        for (cartModel in cartModelList!!) cartSum+= cartModel!!.quantity
+        //badge!!.setNumber(cartSum)
+    }
+
+    override fun onLoadCartFailed(message: String?) {
+        Snackbar.make(lipglossLayout,message!!, Snackbar.LENGTH_LONG).show()
 
     }
 }
